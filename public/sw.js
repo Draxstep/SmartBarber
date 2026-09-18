f42a2d0afe86606ee5e13aab41305bc88b509ce1
Draxstep <113955176+Draxstep@users.noreply.github.com>
2026-09-18 17:36:12 -0500

    Actualizacion del SW para tener respaldo de red

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,37 @@
 const CACHE_NAME = 'smart-barber-v1';
 
+const CORE_ASSETS = [
+  '/',              
+  '/index.html',
+  '/manifest.json',
+  '/favicon.svg'    
+];
+
 self.addEventListener('install', (event) => {
   console.log('[SW] Instalado con éxito');
-  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[SW] Guardando CORE_ASSETS en caché');
+      return cache.addAll(CORE_ASSETS);
+    }).then(() => {
+      return self.skipWaiting();
+    })
+  );
 });
 
 self.addEventListener('activate', (event) => {
   console.log('[SW] Activado y listo para controlar clientes');
-  return self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
 });
